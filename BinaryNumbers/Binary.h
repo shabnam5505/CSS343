ec3950cf2548f41c6a9b67b689ca3dd332d26497
shabnam5505 <shabnambasmani@gmail.com>
2015-01-12 11:59:54 -0800

    Second Commit

---
+++ BinaryNumbers/Binary.h
@@ -0,0 +1,103 @@
+#pragma once
+#include <vector>
+//Shabnam Basmani
+//Basic class allows you to convert from an integer to a binary
+//Setbit and Getbit methods allow you to change the in the binary number
+//One additional helper method that allows you to convert back to an integer may be useful to add to public in the future 
+//most methods are overloaded to allow assignment of binarys and ints to binarys
+#include <iostream>
+using namespace std;
+class Binary
+{
+public:
+	//this is the output operator overload 
+	friend ostream& operator<<(ostream &os, Binary &obj);
+	//this is the input operator overload 
+	friend istream& operator>>(std::istream &inStream, Binary &obj);
+
+	Binary();
+
+	//Precondition: an integer must be passed in
+	//Postcondition: converts the number to binary and stores in the vector
+	Binary(int convertToBinary);
+
+	//Precondition: an integer value is passed in
+	//Postcondition: returns a bool based on the position in vector that was passed in
+	bool getBit(int bitPosition) const;
+
+	//Precondition: an integer value is passed in
+	//Postcondition: sets a bit to true based on the postion in the vector that was passed in
+	void setBit(int bitPosition);
+
+	//Precondition: an integer value is passed in
+	//Postcondition: sets a bit to false based on the position in the vector that was passed in
+	void clearBit(int bitPosition);
+
+	//Precondition: an integer value is passed in
+	//Postcondition: changes the bit to the opposite value based on the position in the vector that was passed in
+	void toggleBit(int bitPosition);
+
+	//copy constructor
+	//Binary(Binary &obj);
+
+	//copy constructor
+	Binary(const Binary &obj);
+
+	//assignment operator overload
+	Binary& operator=(const Binary &source);
+
+	//assignment operator overload from assigning and int to a binary
+	Binary& operator=(int source);
+
+	//+= overload returns a reference to a binary object 
+	Binary& operator+=(Binary& obj);
+	//+ overload returns a binary object 
+	Binary& operator+(Binary& list);
+	//+ overload that takes an binary + int and returns a binary
+	Binary operator+(int obj);
+	//-= overload returns a reference binary object 
+	Binary& operator-=(Binary& obj);
+	//- overload returns a binary object 
+	Binary& operator-(Binary& list);
+	//overload for -operator that takes in a int and returns a binary
+	Binary operator-(int obj);
+	//== overload returns a bool
+	bool operator==(const Binary& obj) const;
+	//!= overload returns a bool
+	bool operator!=(const Binary& obj) const;
+
+
+
+
+	~Binary();
+
+private:
+	//Precondition: pass in an integer
+	//Postcondition: sets the integer to a binary number inside an array
+	void convertToBinary(int num);
+
+	//Precondition: pass in an integer
+	//Postcondition: returns an integer of the size to allocate the array
+	int getSize(int val);
+
+	//Precondition: pass in an array
+	//Postcondition: returns the value of the binary number passed in
+	int convertToInt(bool *arr);
+
+	//stores the value of either positive or negetive for the binary number
+	bool sign;
+	
+	//stores the size of the array
+	int count;
+
+	//this is the array that holds the binary number
+	bool *binary;
+
+	Binary *b;
+	
+
+
+
+
+};
+
---
+++ BinaryNumbers/BinaryNumbers.cpp
@@ -0,0 +1,83 @@
+// BinaryNumbers.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "Binary.h"
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int waitVar;
+	//Binary b(-987);
+
+
+	Binary a;
+	Binary	b(42);
+	Binary c(b);
+	Binary d = -13;
+	Binary x;
+	Binary y = x - 1;
+
+	cout << "This is the new value of Y " << y << endl;
+	a = 27;
+	b = 3;
+	y = 1;
+
+	cout << "a += b" << endl;
+	x = a + b;
+	cout << x << endl;
+
+	cout << a << endl;
+
+	cout << "a -= b" << endl;
+	x = a - b;
+	cout << x << endl;
+
+	
+
+	cout << "Enter integer to be converted to binary:  ";
+	cin >> a;
+	cout << "The binary representation of this number is: " << endl;
+	cout << a << endl;
+	cout << "The 4th bit (four bits away from bit 0) is: " << endl;
+	cout << a.getBit(4) << endl << endl;
+
+	cout << "This is A " << a << endl;
+	cout << "This is C: " << c << endl;
+	cout << "This is B : " << b << endl;
+	cout << "This is D : " << d << endl;
+
+	cout << "Compute c = a + b" << endl;
+	c = a + b;
+	cout << "c = " << c << endl;
+	cout << "Compute d = a + b - c" << endl;
+	d = a + b - c;
+	cout << "d = " << d << endl << endl;
+
+	cout << "Does a == c? " << endl;
+	cout << ((a == c) ? "a == c" : "a != c") << endl << endl;
+
+	cout << "Test assignment operators" << endl;
+	x = c = d = d;    y += a -= b += b;
+	cout << "x = " << x << "    y = " << y << endl << endl;
+
+	a.setBit(-1);			// handle bad input
+	a.setBit(3);			// set 3rd bit (three over from bit zero)
+	a.setBit(1000);			// set 1000th bit; handle as desired, but don't crash
+	a.clearBit(3);			// clear 3rd bit
+	a.clearBit(10000);		// clear 10000th bit
+	a.toggleBit(3);			// toggle 3rd bit
+
+
+
+
+	//b.convertToBinary(10238766565);
+
+	cin >> waitVar;
+	
+
+	return 0;
+}
+
